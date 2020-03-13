95b709627b1428aeb3913f632d56b41837cf1a27
renebreslauer <renebreslauer@me.com>
2020-03-13 11:18:50 -0400

    added category attribute to Events and seeded db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_211017) do
+ActiveRecord::Schema.define(version: 2020_03_13_135102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_03_11_211017) do
     t.bigint "organization_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
     t.index ["organization_id"], name: "index_events_on_organization_id"
   end
 
