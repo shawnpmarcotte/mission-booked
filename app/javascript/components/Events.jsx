3b1a52ab10541e41397d823542b5dada52c12c72
Carolina Klein <carolina.e.klein@gmail.com>
2020-03-15 16:48:52 -0400

    Event information display toggle buttons are now responsive

--- app/javascript/components/Events.jsx
+++ app/javascript/components/Events.jsx
@@ -5,6 +5,7 @@ import Filters from "./Filters";
 const Events = () => {
 const [results, setResults]=useState([]) // order matters in arrays
 const [filters, setFilters]=useState({}) //Object BC it holds a lot of keys and orders don't matter
+const [calendarView, setCalendarView]=useState(false)
 
 useEffect(() => {
   fetchEventData()
@@ -26,29 +27,56 @@ const fetchEventData = () => {
     const updatedFilter = { ...filters, [filterType]: event.target.value }
     setFilters(updatedFilter)
     const queryParams = Object.keys(updatedFilter).reduce((acc, param) => acc ? `${acc}&${param}=${updatedFilter[param]}` : `${param}=${updatedFilter[param]}` ,'')
-    axios.get(`/events.json?${queryParams}`)
-    .then(response => setResults(response.data))
-
+      axios
+      .get(`/events.json?${queryParams}`)
+      .then(response => setResults(response.data))
   }
+
+  const handleThumbnailView = () => {
+    setCalendarView(false)
+  }
+  const handleCalendarView = () => {
+    setCalendarView(true) 
+  }
+
   // spread opperator to map through the filters and maintain them
   //"" [filterType]: "" this is known as a dynamic property, you can pass a string or number
   // and it will assign that property on the object aka first argument on the rocket train
   // and the value of that property would be whatever we selected on the select tag.
-
   return(
-    <div>
-      <Filters handleFilterSelect={handleFilterSelect} /> {/* the curlies around handleFilterSelect represent the end of html, and the start of js */}
-           {
-          results.map((result) => (
-          <div key={result.id}>
-          <p>{result.name}</p>
-          <p>{result.date}</p>
-          <p>{result.about}</p>
-          <p>{result.city_state}</p>
-          </div>
-        ))
+ <>
+      <h1> Events </h1>
+      <div class="viewTypes">
+          <button id="thumbnailView" onClick={handleThumbnailView}>
+              <h2>Thumbnail View</h2>
+          </button>
+          <button onClick={handleCalendarView} id="calendarView">
+              <h2>Calendar View </h2>
+          </button>
+      </div>
+
+      {
+        !calendarView ? (
+          <>
+            <Filters handleFilterSelect={handleFilterSelect} /> 
+              {
+                results.map(result => (
+                  <div key={result.id}>
+                  <p>{result.name}</p>
+                  <p>{result.date}</p>
+                  <p>{result.about}</p>
+                  <p>{result.city_state}</p>
+                  </div>
+                ))
+              }
+            </>
+        ) : 
+          <>
+          <Filters handleFilterSelect={handleFilterSelect} /> 
+           <p>Calendar View</p>
+          </>
       }
-    </div>
+</>
   ) 
 };
 export default Events;
