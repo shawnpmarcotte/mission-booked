fe12dea14036aa95e2ceca01b2d81e910e297188
renebreslauer <renebreslauer@me.com>
2020-03-16 18:09:56 -0400

    event cards

--- app/javascript/components/Events.jsx
+++ app/javascript/components/Events.jsx
@@ -2,34 +2,39 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Filters from './Filters'
 const Events = () => {
+  const [results, setResults] = useState([]) // order matters in arrays
+  const [filters, setFilters] = useState({}) //Object BC it holds a lot of keys and orders don't matter
+  const [calendarView, setCalendarView] = useState(false)
 
-const [results, setResults]=useState([]) // order matters in arrays
-const [filters, setFilters]=useState({}) //Object BC it holds a lot of keys and orders don't matter
-const [calendarView, setCalendarView]=useState(false)
+  useEffect(() => {
+    fetchEventData()
+  }, []) //the empty brakets are dependency arrays, used to break the infinite loop, and only make axios run one
 
-useEffect(() => {
-  fetchEventData()
-}, []) //the empty brakets are dependency arrays, used to break the infinite loop, and only make axios run one
-
-const fetchEventData = () => {
-  axios
-    .get("/events.json")
-    .then(response => {
-      setResults(response.data)
-    })
-    .catch(error => {
-      console.log(error.response);
-    });
-  };
+  const fetchEventData = () => {
+    axios
+      .get('/events.json')
+      .then(response => {
+        setResults(response.data)
+      })
+      .catch(error => {
+        console.log(error.response)
+      })
+  }
 
   // partial applycation = type of function
   // curried = partial application + the other partial application aka double rockets
   const handleFilterSelect = filterType => event => {
     const updatedFilter = { ...filters, [filterType]: event.target.value }
     setFilters(updatedFilter)
-    
-    const queryParams = Object.keys(updatedFilter).reduce((acc, param) => acc ? `${acc}&${param}=${updatedFilter[param]}` : `${param}=${updatedFilter[param]}` ,'')
-      axios
+
+    const queryParams = Object.keys(updatedFilter).reduce(
+      (acc, param) =>
+        acc
+          ? `${acc}&${param}=${updatedFilter[param]}`
+          : `${param}=${updatedFilter[param]}`,
+      ''
+    )
+    axios
       .get(`/events.json?${queryParams}`)
       .then(response => setResults(response.data))
   }
@@ -37,7 +42,7 @@ const fetchEventData = () => {
     setCalendarView(false)
   }
   const handleCalendarView = () => {
-    setCalendarView(true) 
+    setCalendarView(true)
   }
   // spread opperator to map through the filters and maintain them
   //"" [filterType]: "" this is known as a dynamic property, you can pass a string or number
@@ -61,14 +66,20 @@ const fetchEventData = () => {
             filters={filters}
             setFilters={setFilters}
           />
-          {results.map(result => (
-            <div key={result.id}>
-              <p>{result.name}</p>
-              <p>{result.date}</p>
-              <p>{result.about}</p>
-              <p>{result.city_state}</p>
-            </div>
-          ))}
+
+          <div class="cards_container">
+            {results.map(result => (
+              <div class="card" key={result.id}>
+                <p>{result.name}</p>
+                <p>{result.date}</p>
+                <p>{result.city_state}</p>
+                <p class="event_about">{result.about}</p>
+                <p>
+                  <a href="#">View More</a>
+                </p>
+              </div>
+            ))}{' '}
+          </div>
         </>
       ) : (
         <>
