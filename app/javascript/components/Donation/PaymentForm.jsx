6692ae2205f2c64318b6e0d3713139121d500d87
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-03-17 19:48:10 -0400

    donations submission success notice

--- app/javascript/components/Donation/PaymentForm.jsx
+++ app/javascript/components/Donation/PaymentForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "react-credit-cards/es/styles-compiled.css";
 import Cards from "react-credit-cards";
 import validators from "./validators";
-import ThankPage from "./ThankPage";
+import ThankYou from "./ThankPage";
 
 const PaymentForm = ({ results, handleChange, term }) => {
   const INITIAL_STATE = {
@@ -17,6 +17,7 @@ const PaymentForm = ({ results, handleChange, term }) => {
   };
   const [formData, setFormData] = useState(INITIAL_STATE);
   const [errors, setErrors] = useState({});
+  const [donation, setDonation] = useState(false);
 
   const handleInputFocus = field => e => {
     setFormData({ ...formData, [field]: e.target.name });
@@ -32,6 +33,8 @@ const PaymentForm = ({ results, handleChange, term }) => {
     event.preventDefault();
     setErrors(validators(formData, term));
     console.log("worked");
+
+    setDonation(true);
   };
 
   return (
@@ -43,101 +46,111 @@ const PaymentForm = ({ results, handleChange, term }) => {
         name={formData.name}
         number={formData.number}
       />
-      <form onSubmit={handleSubmit}>
-        <input
-          type="number"
-          name="number"
-          placeholder="Card Number"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        {errors.number ? <p>{errors.number}</p> : ""}
-        <input
-          type="text"
-          name="name"
-          placeholder="Full Name"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        {errors.name && <p>{errors.name}</p>}
-        <input
-          type="text"
-          name="expiry"
-          placeholder="Valid thru"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        {errors && <p>{errors.expiry}</p>}
-        <input
-          type="number"
-          name="cvc"
-          placeholder="CVC"
-          onChange={handleInputChange}
-          onFocus={handleInputFocus}
-        />
-        <br />
-        {errors.cvc && <p>{errors.cvc}</p>}
-        <input
-          type="text"
-          name="firstName"
-          placeholder="First Name"
-          onChange={handleInputChange}
-          value={formData.firstName}
-        />
-        <br />
-        {errors.firstName && <p>{errors.firstName}</p>}
-        <br />
-        <input
-          type="text"
-          name="lastName"
-          placeholder="Last Name"
-          onChange={handleInputChange}
-          value={formData.lastName}
-        />
-        <br />
-        {errors.firstName && <p>{errors.firstName}</p>}
-        <br />
-        <input
-          type="number"
-          name="donation"
-          placeholder="$ Donation Amount"
-          onChange={handleInputChange}
-          value={formData.donation}
-          step="0.01"
-        />
-        <br />
-        {errors.donation && <p>{errors.donation}</p>}
-        <br />
-        <div>
-          <label htmlFor="term">Search</label>
-          <br />
+      {donation === false ? (
+        <form onSubmit={handleSubmit}>
           <input
-            type="search"
-            name="term"
-            value={term}
-            onChange={handleChange}
+            type="number"
+            name="number"
+            placeholder="Card Number"
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            required
+          />
+          {errors.number ? <p>{errors.number}</p> : ""}
+          <input
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            required
+          />
+          {errors.name && <p>{errors.name}</p>}
+          <input
+            type="text"
+            name="expiry"
+            placeholder="Valid thru"
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            required
+          />
+          {errors && <p>{errors.expiry}</p>}
+          <input
+            type="number"
+            name="cvc"
+            placeholder="CVC"
+            onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            required
           />
           <br />
+          {errors.cvc && <p>{errors.cvc}</p>}
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            onChange={handleInputChange}
+            value={formData.firstName}
+            required
+          />
+          <br />
+          {errors.firstName && <p>{errors.firstName}</p>}
+          <br />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            onChange={handleInputChange}
+            value={formData.lastName}
+            required
+          />
+          <br />
+          {errors.firstName && <p>{errors.firstName}</p>}
+          <br />
+          <input
+            type="number"
+            name="donation"
+            placeholder="$ Donation Amount"
+            onChange={handleInputChange}
+            value={formData.donation}
+            step="0.01"
+            required
+          />
+          <br />
+          {errors.donation && <p>{errors.donation}</p>}
+          <br />
           <div>
-            {results &&
-              results
-                .filter(result =>
-                  result.name.toLowerCase().includes(term.toLowerCase())
-                )
-                .map((result, i) => {
-                  return (
-                    <p key={i}>
-                      <h1>{result.name}</h1>
-                    </p>
-                  );
-                })}
-            {errors.term && <p>{errors.term}</p>}
+            <label htmlFor="term">Search</label>
+            <br />
+            <input
+              type="search"
+              name="term"
+              value={term}
+              onChange={handleChange}
+              required
+            />
+            <br />
+            <div>
+              {results &&
+                results
+                  .filter(result =>
+                    result.name.toLowerCase().includes(term.toLowerCase())
+                  )
+                  .map((result, i) => {
+                    return (
+                      <p key={i}>
+                        <h1>{result.name}</h1>
+                      </p>
+                    );
+                  })}
+              {errors.term && <p>{errors.term}</p>}
+            </div>
           </div>
-        </div>
-        <button type="submit" onSubmit={ThankPage}>
-          Submit
-        </button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <ThankYou donation={donation} />
+      )}
     </div>
   );
 };
