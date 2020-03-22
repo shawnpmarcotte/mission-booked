c09f7d1f398921b057bba5baed8f840fe0ce89b3
shawnpmarcotte <shawnmarcottejr@gmail.com>
2020-03-22 11:33:12 -0400

    Built in Tile Search Function/Filters

--- app/javascript/components/TileSearch.jsx
+++ app/javascript/components/TileSearch.jsx
@@ -14,42 +14,32 @@ class TitleSearch extends React.Component {
     setCategory = category => {
         this.setState({ category: category });
     };
+    handleSearch = () => {
+        const queryParams = [
+            ["category", this.state.category && this.state.category.value]
+        ].reduce((acc, [paramName, paramValue]) => {
+            if (!paramValue) return acc;
+            const param = `${paramName}=${paramValue}`;
+            return acc ? `${acc}&${param}` : `?${param}`;
+        }, "");
+
+        Turbolinks.visit(`/events${queryParams}`);
+        console.log()
+    };
     render() {
         console.log(localStorage.getItem("userId"));
 
-
-
-        handleSearch = () => {
-            const queryParams = [
-                ["category", this.state.category && this.state.category.value]
-            ].reduce((acc, [paramName, paramValue]) => {
-                if (!paramValue) return acc;
-                const param = `${paramName}=${paramValue}`;
-                return acc ? `${acc}&${param}` : `?${param}`;
-            }, "");
-
-            Turbolinks.visit(`/events${queryParams}`);
-            console.log()
-        };
-
-
-
         return (
 
-            <div class="vol_text_container">
-                <div class="vol_text">
-
-                    <div class="vol"
-                        onClick={this.handleSearch}
-                        type="submit">
-                        Youth
-                    </div>
-                    <div class="vol">Environment</div>
-                    <div class="vol">Rebuilding</div>
-                    <div class="vol">Animals</div>
-                    <div class="vol">Homelessness</div>
+            <div className="vol_text_container">
+                <div className="vol_text">
+                    <a href="/events?category=Animals" ><div className="vol">Animals</div></a>
+                    <a href="/events?category=Environment" ><div className="vol">Environment</div></a>
+                    <a href="/events?category=Youth" ><div className="vol">Youth</div></a>
+                    <a href="/events?category=People" ><div className="vol">People</div></a>
+                    <a href="/events?category=Rebuild" ><div className="vol">Rebuild</div></a>
                 </div>
-            </div>
+            </div >
 
         )
     }
