892febfc20d82a38ebce174bf9f9ca87ed419c7e
renebreslauer <renebreslauer@me.com>
2020-03-22 23:04:51 -0400

    centered slider

--- app/javascript/components/Slider.jsx
+++ app/javascript/components/Slider.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-import img1 from "../../assets/images/pollutionslider.jpg";
-import img2 from "../../assets/images/pollutionslider_after.jpg";
-import ReactCompareImage from "react-compare-image";
-
+import React from 'react'
+import img1 from '../../assets/images/pollutionslider.jpg'
+import img2 from '../../assets/images/pollutionslider_after.jpg'
+import ReactCompareImage from 'react-compare-image'
 const Slider = () => {
   return (
-    <div>
-      <div style={{ maxWidth: "640px" }}>
+    <div
+      style={{
+        margin: 'auto',
+        padding: '20px',
+        width: '50%'
+      }}
+    >
+      <div style={{ maxWidth: '60rem' }}>
         <ReactCompareImage leftImage={img1} rightImage={img2} hover />
       </div>
     </div>
-  );
-};
-
-export default Slider;
+  )
+}
+export default Slider
