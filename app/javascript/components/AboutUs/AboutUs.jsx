9b07cd865cd086c641508e2ee538a7ae1069a377
josedani08200 <jose.dani08200@gmail.com>
2020-04-10 16:34:11 -0400

    fixing css

--- app/javascript/components/AboutUs/AboutUs.jsx
+++ app/javascript/components/AboutUs/AboutUs.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useSpring, animated } from "react-spring";
-import "./styles.css";
 import github from "./logos/github.jpg";
 import LinkedIn from "./logos/LinkedIn-Logo.png";
 
 const calc = (x, y) => [
   -(y - window.innerHeight / 2) / 20,
   (x - window.innerWidth / 2) / 20,
-  1.1
+  1.1,
 ];
 const trans = (x, y, s) =>
   `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
@@ -15,19 +14,19 @@ const trans = (x, y, s) =>
 const AboutUs = () => {
   const [shawn, setShawn] = useSpring(() => ({
     xys: [0, 0, 1],
-    config: { mass: 5, tension: 350, friction: 40 }
+    config: { mass: 5, tension: 350, friction: 40 },
   }));
   const [rene, setRene] = useSpring(() => ({
     xys: [0, 0, 1],
-    config: { mass: 5, tension: 350, friction: 40 }
+    config: { mass: 5, tension: 350, friction: 40 },
   }));
   const [jose, setJose] = useSpring(() => ({
     xys: [0, 0, 1],
-    config: { mass: 5, tension: 350, friction: 40 }
+    config: { mass: 5, tension: 350, friction: 40 },
   }));
   const [carolina, setCarolina] = useSpring(() => ({
     xys: [0, 0, 1],
-    config: { mass: 5, tension: 350, friction: 40 }
+    config: { mass: 5, tension: 350, friction: 40 },
   }));
   return (
     <>
